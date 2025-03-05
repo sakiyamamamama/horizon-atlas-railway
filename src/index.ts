001482d80a630f8c7c32503f049c094c7a686e5c
sasasanono <ukakau@gmail.com>
2025-03-05 22:01:11 +0900

    環境変数を文字列に

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { getRole } from "./DiscordApi";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const allowedOrigins = [process.env.FRONTEND_URL2, process.env.FRONTEND_URL, process.env.local_url];
+const allowedOrigins = ["https://ryukoku-horizon.github.io/horizon-atlas", process.env.FRONTEND_URL, process.env.local_url];
 
 app.use(cor.default({
   origin: (origin, callback) => {
